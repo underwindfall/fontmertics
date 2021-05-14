44f689e55f871034db8771f6a3f0de071a47189e
Qifan Yang <14819756+underwindfall@users.noreply.github.com>
2021-05-14 09:54:40 +0200

    :tada: init project

---
+++ app/src/main/java/com/qifan/fontmetrics/FontMetricsView.kt
@@ -0,0 +1,14 @@
+package com.qifan.fontmetrics
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * an example to introduce u how to use font metrics
+ */
+class FontMetricsView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+}
